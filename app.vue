0feb3d2da2988d23d833592bf7c88c64709d23bb
marcusfu <marcussfu@gmail.com>
2023-07-31 17:29:25 +0800

    eslint and prettier setting and vue-tsc

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <h1>Hello World, {{title}}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const year = 2023
+const title = `${year} test`
+</script>
