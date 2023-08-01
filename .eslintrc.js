bc2cebfe689988140d432dea19ccac77d0c116e1
marcusfu <marcussfu@gmail.com>
2023-08-01 14:42:07 +0800

    adjust eslintrc for error  Delete ␍  prettier/prettier and add tailwind config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error'
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
   },
   overrides: [
     {
