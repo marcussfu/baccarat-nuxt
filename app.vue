7e986ac29f2bc4fc02f7dc752a2e1dc44fefd8a8
marcusfu <marcussfu@gmail.com>
2023-08-01 11:32:52 +0800

    add nuxtjs/tailwindcss

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">Hello World</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">{{ title }}</p>
+    </div>
     <!-- <NuxtWelcome /> -->
-    <h1>Hello World, {{title}}</h1>
   </div>
 </template>
 
